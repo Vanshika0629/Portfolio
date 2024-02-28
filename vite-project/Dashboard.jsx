5fcf146092058844ecb15854db627d8297e01707
Vanshika0629 <snnvanshika1524@gmail.com>
2024-02-28 10:49:43 +0530

    |modified|

---
+++ vite-project/Dashboard.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <>
+      <div class="container">
+        <div class="card">
+          <div class="card-body">
+            <div className="row">
+              <div class="col-8">
+                <h2>About myself</h2>
+                <p> I am vanshika. I pursuing my graduation from Khalsa college for women.</p>
+              </div>
+              <div class="col-4">
+              <img src="https://th.bing.com/th/id/OIP.0iG7xinjnxYwduQCGJs1rQHaKX?w=187&h=261&c=7&r=0&o=5&dpr=1.5&pid=1.7"/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
